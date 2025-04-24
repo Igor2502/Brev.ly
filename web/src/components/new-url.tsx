4843c5946e4a81e2d85857b094a41f2fca2629e3
Igor <igor.gds.00@gmail.com>
2025-04-24 18:36:49 -0300

    feat(front): adicionar pasta de assets e incluir logo no componente NewUrl

--- web/src/components/new-url.tsx
+++ web/src/components/new-url.tsx
@@ -1,14 +1,18 @@
+import logoBrevly from "../assets/Logo.svg";
 import { Button } from "./button";
 import { Header } from "./header";
 import { Input } from "./input";
 
 export function NewUrl() {
 	return (
-		<div className="flex flex-col bg-gray-100 max-w-[360px] h-[340px] w-full rounded-2xl p-8 justify-between">
-			<Header />
-			<Input />
-			<Input />
-			<Button />
+		<div className="flex flex-col gap-2 max-w-[360px] w-full">
+			<img src={logoBrevly} alt="Brevly Logo" className="w-24 h-24 mx-auto md:mx-0" />
+			<div className="flex flex-col bg-gray-100 h-[340px] rounded-2xl p-8 justify-between">
+				<Header />
+				<Input />
+				<Input />
+				<Button />
+			</div>
 		</div>
 	);
 }
