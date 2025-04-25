a021167dcf08f4943112bdecdec61bd345e1d8bc
Igor <igor.gds.00@gmail.com>
2025-04-25 19:46:14 -0300

    refactor(front): ajustar estilização do componente Button

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -1,6 +1,9 @@
 export function Button() {
   return (
-    <button className="bg-blue-base text-white rounded-md h-12 w-full text-md">
+    <button
+      type="button"
+      disabled={true}
+      className="bg-blue-base hover:bg-blue-dark text-white rounded-md h-12 w-full text-md disabled:opacity-50 disabled:pointer-events-none aria-disabled:opacity-50 aria-disabled:pointer-events-none">
       Salvar link
     </button>
   )
