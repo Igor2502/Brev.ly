ddfdc6756681d42a510a1e1cab4811513a20e678
Igor <igor.gds.00@gmail.com>
2025-04-29 18:37:01 -0300

    style(front): aplica estilização da pagina 404

--- web/src/pages/NotFound.tsx
+++ web/src/pages/NotFound.tsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
+import notFound from "../assets/404.svg";
 
 export function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-dvh">
-      <h1 className="text-3xl font-bold text-red-600">404</h1>
-      <p className="text-gray-600">Ops! Página não encontrada.</p>
-      <Link to="/" className="mt-4 text-blue-500 hover:underline">
-        Voltar para Home
-      </Link>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className='flex flex-col items-center justify-center max-h-[329px] max-w-[580px] bg-gray-100 px-12 py-16 gap-6'>
+        <img src={notFound} alt="404" className="w-48 h-20" />
+        <h1 className="text-xl text-gray-600">
+          Link não encontrado
+        </h1>
+        <p className="text-md text-gray-500">
+          O link que você está tentando acessar não existe, foi removido ou é uma URL inválida. Saiba mais em&nbsp;
+          <Link to="/" className="text-blue-500 hover:underline">
+            Brev.ly
+          </Link>.
+        </p>
+      </div>
     </div>
   );
 }
