53d6bb4ce1bad7380de5948e43dde44ca1f40a28
Igor <igor.gds.00@gmail.com>
2025-05-01 15:39:47 -0300

    feat(front): exporta listagem de URLs para CSV

--- web/src/components/my-urls.tsx
+++ web/src/components/my-urls.tsx
@@ -1,12 +1,29 @@
 import { DownloadSimple } from '@phosphor-icons/react';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../shared/api-fetch';
 import { useUrls } from '../store/urls';
 import { ListUrls } from "./list-urls";
 import { Button } from "./ui/button";
 import { Header } from "./ui/header";
 
 export function MyUrls() {
+  const [isLoading, setIsLoading] = useState(false);
   const { urls } = useUrls();
   const hasData = urls.size > 0;
+
+  async function exportUrlsToCSV() {
+    setIsLoading(true);
+    try {
+      const response = await api.get('/export-urls-to-csv');
+      if (response.status === 200) {
+        window.open(response.data.url, '_blank');
+      }
+    } catch (error) {
+      toast('Erro ao exportar os links', { type: 'error' });
+    }
+    setIsLoading(false);
+  }
 
   return (
     <div className="flex flex-col gap-2 max-w-[366px] md:max-w-[580px] w-full bg-gray-100 rounded-2xl p-6 md:p-8">
@@ -17,7 +34,8 @@ export function MyUrls() {
           size="medium"
           label="Baixar CSV"
           icon={<DownloadSimple size={16} className="text-gray-600" />}
-          disabled={!hasData}
+          disabled={!hasData || isLoading}
+          onClick={exportUrlsToCSV}
         />
       </div>
 
